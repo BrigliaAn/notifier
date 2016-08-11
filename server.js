29a103c4ac05836e78481118aa9ebafd207fee4b
antonella <briglia.antonella@gmail.com>
2016-08-11 10:29:43 -0300

    cambio de puerto

--- server.js
+++ server.js
@@ -3,28 +3,35 @@ const app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var Notification = require('./model/notification.js');
+
 router.use(function(req,res,next){
 	console.log('/'+req.method);
 	next();
 });
 
-router.get('/admin',function(req,res){
+router.get('/',function(req,res){
 	res.sendFile(path +'admin.html');
 });
 
 router.get('/index',function(req,res){
 	res.sendFile(path + 'index.html');
-	console.log(path+'index.html')
 });
 
 router.get('/create',function(req,res){
 	res.sendFile(path + 'create.html');
-	console.log(path+'create.html')
 });
 
 router.get('/list',function(req,res){
 	res.sendFile(path + 'list.html');
-	console.log(path+'list.html')
+});
+
+router.post('/createNotification',function(req,res){
+	res.sendFile(path + 'list.html');
 });
 
 app.use('/',router);
@@ -34,9 +41,11 @@ app.use(express.static(__dirname + '/public'));
 // 	res.sendFile(path + "404.html");
 //});
 
-app.listen(3000,function(){
-	console.log('Listening on port 3000');
-})
+app.listen(process.env.PORT || 5000);
+
+//app.listen(3000,function(){
+//	console.log('Listening on port 3000');
+//})
 
 
 
